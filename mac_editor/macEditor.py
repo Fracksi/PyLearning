0cf42f10159208375a6188403b4e5d52dba19ff1
Fracksi <STFaust2@proton.me>
2023-12-16 00:21:28 -0600

    Changes after initial checks and MAN reads

--- mac_editor/macEditor.py
+++ mac_editor/macEditor.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call("ip addr", shell=True)
+subprocess.call("ip link set eth0 down", shell=True)
+subprocess.call("ip link set eth0 address 00:11:22:33:44:66", shell=True)
+subprocess.call("ip link set eth0 up", shell=True)
