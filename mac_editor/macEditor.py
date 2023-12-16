4d476931d4919ad2c5e571918610c913585ae985
Fracksi <STFaust2@proton.me>
2023-12-16 00:43:26 -0600

    Fleshing out the beginner steps.

--- mac_editor/macEditor.py
+++ mac_editor/macEditor.py
@@ -2,6 +2,18 @@
 
 import subprocess
 
-subprocess.call("ip link set eth0 down", shell=True)
-subprocess.call("ip link set eth0 address 00:11:22:33:44:66", shell=True)
-subprocess.call("ip link set eth0 up", shell=True)
+# Variables
+
+userInterface = "eth0"
+newMacAddress = "00:11:22:33:44:66"
+
+# Print output overview
+print("[+] Changing MAC Address for " + userInterface + " to " + newMacAddress)
+
+# Make system calls
+subprocess.call("ip link set " + userInterface + " down", shell=True)
+subprocess.call("ip link set " + userInterface + " address " + newMacAddress, shell=True)
+subprocess.call("ip link set " + userInterface + " up", shell=True)
+
+# Checks
+subprocess.call("ip addr", shell=True)
