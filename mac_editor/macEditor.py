c93ecee03cde1c200ad11f15224e6492a28f4934
Fracksi <STFaust2@proton.me>
2023-12-19 23:46:00 -0600

    Fixing it up a bit with more proper coding methods

--- mac_editor/macEditor.py
+++ mac_editor/macEditor.py
@@ -2,17 +2,23 @@
 
 import subprocess
 
-# Variables
-userInterface = input("Please enter interface to change: ")
-newMacAddress = input("Enter the new MAC: ")
+def main():
 
-# Print output overview
-print("[+] Changing MAC Address for " + userInterface + " to " + newMacAddress)
+  # Variables
+  userInterface = input("Please enter interface to change: ")
+  newMacAddress = input("Enter the new MAC: ")
 
-# Make system calls
-subprocess.call(["ip", "link", "set", userInterface, "down"])
-subprocess.call(["ip", "link", "set", userInterface, "address", newMacAddress])
-subprocess.call(["ip", "link", "set", userInterface, "up"])
+  # Print output overview
+  print("[+] Changing MAC Address for " + userInterface + " to " + newMacAddress)
 
-# Checks
-subprocess.call(["ip", "addr"])
+  # Make system calls
+  subprocess.call(["ip", "link", "set", userInterface, "down"])
+  subprocess.call(["ip", "link", "set", userInterface, "address", newMacAddress])
+  subprocess.call(["ip", "link", "set", userInterface, "up"])
+
+  # Checks
+  subprocess.call(["ip", "addr"])
+
+# Technically more proper
+if __name__ == '__main__':
+  main()
