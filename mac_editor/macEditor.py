bfa3f52e5a5694402583b403eb645869a6e44a12
Fracksi <STFaust2@proton.me>
2023-12-26 00:56:10 -0600

    Added basic error handling for missing inputs

--- mac_editor/macEditor.py
+++ mac_editor/macEditor.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 
+
 import subprocess
 import optparse
+
+
+def argumentGrabbing():
+  # Loads child for OptionsParser and assigns basic inline flags
+  parser = optparse.OptionParser()
+  parser.add_option("-i", "--interface", dest="userInterface", help="Interface where the MAC is changed.")
+  parser.add_option("-m", "--mac", dest="newMacAddr", help="New MAC being assigned.")
+
+  # Method that returns the options from the args
+  (options, arguments) = parser.parse_args()
+
+  #Basic error handling if -i or -m aren't provided.
+  if not options.userInterface:
+    parser.error("[-] IP Interface not included, use --help for more info.")
+  elif not options.newMacAddr:
+    parser.error("[-] New MAC not included, use --help for more info.")
+  return options
 
 
 def macSwitcharoo(userInterface, newMacAddr):
@@ -18,15 +36,10 @@ def macSwitcharoo(userInterface, newMacAddr):
 
 
 def main():
+  # Grabs what to send from function above, basic error handling.
+  options = argumentGrabbing()
 
-  # Loads child for OptionsParser and assigns basic inline flags
-  parser = optparse.OptionParser()
-  parser.add_option("-i", "--interface", dest="userInterface", help="Interface where the MAC is changed.")
-  parser.add_option("-m", "--mac", dest="newMacAddr", help="New MAC being assigned.")
-
-  # Method that returns the options from the args
-  (options, arguments) = parser.parse_args()
-
+  # Sticks the info the script wants into the function.
   macSwitcharoo(options.userInterface,options.newMacAddr)
 
 
